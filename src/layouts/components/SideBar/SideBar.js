c239e29195b4ccdda0e5a1f9f4ea56d3b2c83850
NgocDangVan <ngocdangvan98@gmail.com>
2024-09-27 15:09:58 +0700

    Add sidebar ui

---
+++ src/layouts/components/SideBar/SideBar.js
@@ -0,0 +1,19 @@
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+import Menu, {MenuItem} from './Menu';
+import {HomeIcon, HomeActiveIcon, UserGroupIcon, UserGroupActiveIcon, LiveIcon, LiveActiveIcon} from '~/components/Icons';
+import config from '~/config';
+
+const cx = classNames.bind(styles);
+
+function SideBar() {
+    return <aside className={cx('wrapper')}>
+               <Menu>
+                    <MenuItem title="For you" to={config.routes.home} icon={<HomeIcon></HomeIcon>} activeIcon = {<HomeActiveIcon></HomeActiveIcon>}></MenuItem>
+                    <MenuItem title="Following" to={config.routes.following} icon={<UserGroupIcon></UserGroupIcon>} activeIcon = {<UserGroupActiveIcon></UserGroupActiveIcon>}></MenuItem>
+                    <MenuItem title="Live" to={config.routes.live} icon={<LiveIcon></LiveIcon>}  activeIcon = {<LiveActiveIcon></LiveActiveIcon>}></MenuItem>
+               </Menu>
+           </aside>;
+}
+
+export default SideBar;
